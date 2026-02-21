b884866211d1990178be7cd16795910627f36028
Jonas Mühlbach <jonas.muehlbach@googlemail.com>
2026-02-21 20:48:31 +0100

    Add files via upload
    New feature user gets a output on the terminal to choose from

--- Program.vb
+++ Program.vb
@@ -2,6 +2,14 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        'The user is given the opportunity to choose from various options
+        Console.Writeline("Please choose on of the options")
+        Console.Writeline("(1) New user") 
+        Console.Writeline("(2) Book variety ") 
+        Console.Writeline("(3) All Users")
+        Console.Writeline("(4) Borrow a book (ISBN)")
+        Console.Writeline("(5) Give Back (ISBN)")
+        Console.Writeline("(6) Borrowed books by user")
+        Console.Writeline("(7) Close")
     End Sub
 End Module
