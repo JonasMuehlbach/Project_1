0ed6cc3d881f392ab5767cda8c67797470690bf7
Jonas Mühlbach <jonas.muehlbach@googlemail.com>
2026-02-24 11:32:06 +0100

    Add files via upload
    Implemented Case 2 and 3 with the Var. of all Users and Books in the Library

--- Program.vb
+++ Program.vb
@@ -2,6 +2,18 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+        'Creating a Var. that has all the books in
+         Dim LibraryData As String =
+                            "978-3-16-148410-0;Einführung in die Informatik; Müller; verfügbar|" & vbCrLf &
+                            "978-0-13-110362-7;Programmieren mit VB.NET; Schneider; verfügbar|" & vbCrLf &
+                            "978-3-540-69006-6;Grundlagen der Softwaretechnik;Meier; ausgeleiehen|" & vbCrLf &
+                            "978-3-642-05445-3;Datenstrukturen und Algorithmen;Klein; verfügbar|"
+        'Creating a Var. that has all the Users in
+        Dim User As String =
+                            "U001; Max Mustermann|" & vbCrLf &
+                            "U002; Erika Musterfrau|" & vbCrLf &
+                            "U003; Hans Meier|" & vbCrLf &
+                            "U004; Laura Schmidt|"
         'The user is given the opportunity to choose from various options
         Console.Writeline("Please choose on of the options")
         Console.Writeline("(1) New user") 
@@ -11,5 +23,26 @@ Module Program
         Console.Writeline("(5) Give Back (ISBN)")
         Console.Writeline("(6) Borrowed books by user")
         Console.Writeline("(7) Close")
+        Console.Write("imput: ")
+            'Imput gets read prep for further steps 
+            Dim imput As String = Console.ReadLine()
+
+            Select Case imput
+                    Case "1"
+                        Console.Writeline("imput was 1")
+                    Case "2"
+                        Console.Writeline(LibraryData)
+                    Case "3"
+                        Console.Writeline(User)
+                    Case "4"
+                        Console.Writeline("imput was 4")
+                    Case "5"
+                        Console.Writeline("imput was 5")
+                    Case "6"
+                        Console.Writeline("imput was 6")
+                    Case "7"
+                        Console.Writeline("imput was 7")
+            'can get extendet till case 7 wit the programm that should get run under that case
+            End Select      
     End Sub
 End Module
